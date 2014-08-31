ce9bd3f87c4e7e29436763b4017a4d5d34b075c7
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-08-31 10:18:43 +0200

    fixed heroku typo

--- src/main/java/com/getbro/api/Heroku.java
+++ src/main/java/com/getbro/api/Heroku.java
@@ -7,7 +7,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
  * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
  * command that is used for launching should fire this main method.
  */
-public class Main {
+public class Heroku {
 
     public static void main(String[] args) throws Exception{
         final Server server = new Server(80);
