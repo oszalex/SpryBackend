c9e60a40638445ee46165742fee47473ae25bfe1
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-11-19 15:50:39 +0100

    change to tomcat and war deployment

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -2,13 +2,21 @@ package rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
+    // Used when deploying to a standalone servlet container
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
